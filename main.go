b515ebb2af8641f0cfc88b56e9863cae49f7a02d
Vasistinitha <148735165+Vasistinitha@users.noreply.github.com>
2023-12-15 15:19:17 -0500

    Add files via upload

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"sensor-api/controller"
+	"sensor-api/weatherapi"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	weatherapi.LiveWeatherApi()
+
+	router := gin.Default()
+	router.GET("/sensor", controller.GetSensorData)
+	router.GET("/weather", controller.GetWeatherData)
+	router.GET("/predictions", controller.GetPredictions)
+	port := os.Getenv("PORT")
+	fmt.Print(port)
+
+	if port == "" {
+		port = "3000"
+	}
+	router.Run(":" + port)
+}
